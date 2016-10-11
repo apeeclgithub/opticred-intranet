cdb2a8e00222904da8c71bf4b64cd180d81f9b86
Pablo Hernández <devphernandez@gmail.com>
2016-10-11 17:09:34 -0300

    CAPTADOR TERMINADO

--- application/model/classCaptador.php
+++ application/model/classCaptador.php
@@ -10,7 +10,7 @@
 
 			$objConn = new Database();
 			$sql = $objConn->prepare('	SELECT CAP_ID, CAP_NAME, CAP_PHONE
-										FROM CAPTADOR 
+										FROM CAPTADOR
 										WHERE CAP_ACTIVE = 1
 										GROUP BY CAP_ID');
 
@@ -20,12 +20,31 @@
 			return $this->captador;
 
 		}
+		
+		public function traeMonto($id){
+			$objConn = new Database();
+			$sql = $objConn->prepare('	SELECT COALESCE(SUM(COM_TOTAL), 0)
+										FROM COMISION
+										WHERE CAPTADOR_CAP_ID = :id ');
+
+			
+			$sql->bindParam(':id', $id);
+			
+			$this->captador = $sql->execute();
+			$this->captador = $sql->fetchAll(PDO::FETCH_ASSOC);
+			foreach((array)$this->captador as $yo){
+				foreach((array)$yo as $clave){
+					return $this->captador = $clave;
+				}
+			}
+			
+		}
 
 		public function selectCaptador($capName){
 
 			$objConn = new Database();
 			$sql = $objConn->prepare('	SELECT 	CAP_ID 
-										FROM 	captador 
+										FROM 	CAPTADOR 
 										WHERE 	CAP_NAME = :capName
 										AND 	CAP_ACTIVE = 1');
 
@@ -41,17 +60,17 @@
 		public function addCaptador($capName, $capPhone){
 			
 			$objConn = new Database();
-			$sql = $objConn->prepare('INSERT INTO captador (CAP_NAME, CAP_PHONE) 
-										VALUES (:capName, :capPhone)');
+			$sql = $objConn->prepare('INSERT INTO CAPTADOR (CAP_NAME, CAP_PHONE, CAP_ACTIVE) 
+										VALUES (:capName, :capPhone, 1)');
 		
 			$sql->bindParam(':capName', $capName);
 			$sql->bindParam(':capPhone', $capPhone);
-/*
-			if(!(array)$this->selectCaptador($capName)){
-				$this->captador = $sql->execute();
-				$this->firstLoad();
-			}
-*/
+
+			// if(!(array)$this->selectCaptador($capName)){
+				// $this->captador = $sql->execute();
+				// $this->firstLoad();
+			// }
+
 			$this->captador = $sql->execute();
 			return $this->captador;
 
@@ -60,8 +79,8 @@
 		public function firstLoad(){
 
 			$objConn = new Database();
-			$sql = $objConn->prepare('INSERT INTO comision (com_total, CAP_ID) 
-										VALUES (0, (SELECT CAP_ID FROM captador ORDER BY CAP_ID DESC LIMIT 1))');
+			$sql = $objConn->prepare('INSERT INTO COMISION (COM_TOTAL, CAPTADOR_CAP_ID) 
+										VALUES (0, (SELECT CAP_ID FROM CAPTADOR ORDER BY CAPTADOR_CAP_ID DESC LIMIT 1))');
 			$this->captador = $sql->execute();
 			return $this->captador;
 		}
@@ -69,7 +88,7 @@
 		public function activateCaptador($capName, $capPhone){
 
 			$objConn = new Database();
-			$sql = $objConn->prepare('	UPDATE captador 
+			$sql = $objConn->prepare('	UPDATE CAPTADOR 
 										SET	CAP_ACTIVE = 1,
 											CAP_PHONE = :capPhone
 										WHERE CAP_NAME = :capName');
@@ -86,7 +105,7 @@
 		public function updateCaptador($capId, $capName, $capPhone){
 
 			$objConn = new Database();
-			$sql = $objConn->prepare('	UPDATE captador 
+			$sql = $objConn->prepare('	UPDATE CAPTADOR 
 										SET CAP_NAME = :capName, 
 											CAP_PHONE = :capPhone
 										WHERE CAP_ID = :capId');
@@ -105,7 +124,7 @@
 		public function delCaptador($capId){
 
 			$objConn = new Database();
-			$sql = $objConn->prepare('	UPDATE captador 
+			$sql = $objConn->prepare('	UPDATE CAPTADOR 
 										SET	CAP_ACTIVE = 0
 										WHERE CAP_ID = :capId');
 
@@ -117,5 +136,4 @@
 		}
 
 	}
-
 ?>
